885a2d2969dcce44a19e65c5b63dd52a055bbd5a
Gruppo IV SE <f4ntase@gmail.com>
2017-06-29 14:21:02 +0200

    Add files via upload
    Primo driver è andato, anche con doc interna al momento da caricare

--- Tutorial_7_kernel/design/nodriver/nodriver_header.h
+++ Tutorial_7_kernel/design/nodriver/nodriver_header.h
@@ -31,9 +31,9 @@
 #define MASK_SIZE (~(PAGE_SIZE-1))		/*!< Bit mask to extract page address, without offset */
 
 /* Function prototypes -------------------------------------------------------*/
-void usage(char *name);
-int parse_command(int argc,char **argv,int* address,int* r_w,int* value);
-int open_memory(int* fd,int phy_address, int* page_offset,void** virtual_address);
-void read_gpio(int* value,void* virtual_address,int page_offset);
-void write_gpio(int value,int phy_address,void* virtual_address, int page_offset);
-void close_memory(int fd, void* virtual_address);
+void usage(char *name);	/*!< usage function is called to help the user */
+int parse_command(int argc,char **argv,int* address,int* r_w,int* value); /*!< parse_command function is called to parse arguments passed to driver */
+int open_memory(int* fd,int phy_address, int* page_offset,void** virtual_address);/*!< open_memory function is called to open memory file */
+void read_gpio(int* value,void* virtual_address,int page_offset);/*!< read_gpio function is called to do read operation from GPIO */
+void write_gpio(int value,int phy_address,void* virtual_address, int page_offset);/*!< write_gpio function is called to do write operation on GPIO */
+void close_memory(int fd, void* virtual_address);/*!< close_memory function is called to close memory file */
